31d99eb1fec91032692d33bd012a85995adc3539
nccvector <fzn1020@gmail.com>
2023-05-12 20:04:12 +0900

    remove trash

--- PhongData.h
+++
@@ -1,9 +0,0 @@
-//
-// Author: Faizan Ali (github.com/nccvector)
-// Date: 23/05/05
-//
-
-#ifndef OPENGL_FROM_SCRATCH_PHONGDATA_H
-#define OPENGL_FROM_SCRATCH_PHONGDATA_H
-
-#endif // OPENGL_FROM_SCRATCH_PHONGDATA_H
